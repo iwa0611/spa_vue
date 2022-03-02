99dcb74cb8ffe761051f9f1fe26a392b7bb0a546
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-03-02 09:27:38 +0900

    [add]store追加。ログイン・ログアウト処理をミューテーションに追加。セッション保存pluginsn追加

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,52 @@
-import Vue from 'vue'
+import Vue  from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    id: 0
+    userData: '',
+    err: ''
   },
   mutations: {
+    // login時の処理
+    login: async function( state, {email, passWord}) {
+      axios.defaults.baseURL = process.env.VUE_APP_API_URL
+      await axios.post('/auth/sign_in', { email: email, password: passWord })
+      // ストレージにresを保存
+      .then(res => {(state.userData = res)
+            sessionStorage.setItem('userData', JSON.stringify({ userData: state.userData }))
+            })
+      // エラーメッセージ出力
+      .catch(e => state.err = e.response.data.errors)
+    },
+    // logout時の処理
+    logout: async function( state, {token, client, expiry, uid, type}) {
+      axios.defaults.baseURL = process.env.VUE_APP_API_URL
+      await axios.delete('/auth/sign_out', { headers: { 'access-token': token,                              
+                                                        client: client,
+                                                        expiry: expiry,
+                                                        uid: uid,
+                                                        'token-type': type
+                                                      }})
+      .then(() => {
+            // ストレージ情報削除
+            sessionStorage.removeItem('userData')
+            // ストアの値リセット
+            state.userData = '', state.err = ''
+            })
+      .catch(e => state.err = e.response.data.errors)
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState(
+    { key: 'userData',
+      storage: window.sessionStorage
+    } // 保存先をsessionに変更
+  )] 
 })
